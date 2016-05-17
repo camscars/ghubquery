61ddc656d3a8e057896f778a21a482d3aeaed53e
camscars <camscars702@gmail.com>
2016-05-16 17:40:10 -0700

    App to query Github for user data

---
+++ query/urls.py
@@ -0,0 +1,9 @@
+# app/urls.py
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+	url(r'^profile/$', views.profile, name='profile'),
+]
---
+++ query/views.py
@@ -0,0 +1,26 @@
+# views.py 
+
+from django.shortcuts import render, HttpResponse
+import requests
+
+# Create your views here.
+
+def profile(request):
+    parsedData = []
+    if request.method == 'POST':
+        username = request.POST.get('user')
+        req = requests.get('https://api.github.com/users/' + username)
+        jsonList = []
+        jsonList.append(req.json())
+        userData = {}
+        for data in jsonList:
+            userData['name'] = data['name']
+            userData['blog'] = data['blog']
+            userData['email'] = data['email']
+            userData['public_gists'] = data['public_gists']
+            userData['public_repos'] = data['public_repos']
+            userData['avatar_url'] = data['avatar_url']
+            userData['followers'] = data['followers']
+            userData['following'] = data['following']
+        parsedData.append(userData)
+    return render(request, 'query/profile.html', {'data': parsedData})
